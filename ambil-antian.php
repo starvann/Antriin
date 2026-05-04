f35207d38a810cb83748bf36ababbd6a62a233e5
Vanya et Aza <156308576+starvann@users.noreply.github.com>
2026-05-04 13:11:10 +0700

    include

--- ambil-antian.php
+++ ambil-antian.php
@@ -1,9 +1,5 @@
 <?php
-$conn = mysqli_connect("127.0.0.1", "root", "", "admin_antrian");
-
-if (!$conn) {
-    die("Koneksi gagal: " . mysqli_connect_error());
-}
+include './koneksi.php';
 
 if (isset($_POST['ambil'])) {
 
